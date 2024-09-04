790c64d26dfd56451587fb8c9e9293c70ad866b4
faheem59 <faheem@binmile.com>
2024-09-04 12:38:39 +0530

    ci/cd added

---
+++ user_process/jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    testMatch: ['<rootDir>/__tests__/**/*.test.js'],  // Adjust path as needed
+    // Optionally, if using TypeScript:
+    // testMatch: ['<rootDir>/__tests__/**/*.test.ts'],
+    testEnvironment: 'node',  // Or 'jsdom' for browser-like environment
+};
